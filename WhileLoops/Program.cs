29c850aec00bdd3eda3bb5c9ae057aa7d21731b5
Brittanie Campos <bri2212040@maricopa.edu>
2022-06-22 12:33:19 -0700

    Add project files.

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,13 @@
+﻿// while loop = repeats some code while some condition remains true
+
+String name = "";
+
+while( name == "")
+{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine("Hello " + name);
+
+Console.ReadKey();
